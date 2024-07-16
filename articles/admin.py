c6aa6ed9a6646391ff224f37a15dc3072e10ddc8
Doomcaster1917 <Webtalestoday@gmail.com>
2024-07-16 20:01:46 +0300

    edited admin datetime

--- articles/admin.py
+++ articles/admin.py
@@ -7,9 +7,9 @@ class ArticleAdmin(admin.ModelAdmin):
     model = Article
     fields = ('name', 'slug', 'title', 'seo_description',
               'seo_keywords', 'full_description',
-              'image', 'priority', 'image_tag', 'datetime')
-    readonly_fields = ('image_tag',)
-    list_display = ('name', 'title', 'image_tag',)
+              'image', 'priority', 'image_tag')
+    readonly_fields = ('image_tag', 'datetime')
+    list_display = ('name', 'title', 'image_tag', 'datetime')
 
     def image_tag(self, obj):
         return format_html(f'<img src="{obj.image.url if obj.image else ""}" max-width = "100" height = "100">')
