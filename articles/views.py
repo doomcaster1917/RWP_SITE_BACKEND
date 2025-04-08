74e2b3852caa8fa422d49dc4b4a2893865f23cb2
Doomcaster1917 <Webtalestoday@gmail.com>
2025-04-08 13:53:07 +0300

    Added markdown coverter

--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,8 @@ from .models import Article
 from rest_framework.decorators import api_view
 from .serializers import ArticleTinySerializer, ArticleFullSerializer
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
+from markdownify import markdownify as md
+
 
 @api_view(["GET"])
 def ArticlesView(request):
@@ -16,8 +17,12 @@ def ArticleFullView(request):
     object_list = Article.objects.filter(slug=request.GET["slug"])
     serializer = ArticleFullSerializer(object_list, many=True)
 
+    data = serializer.data
+    for item in data:
+        item['content'] = md(item['content'])
+
     if object_list:
-        return Response(serializer.data)
+        return Response(data)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
